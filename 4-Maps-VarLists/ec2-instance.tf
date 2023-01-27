0b232083317a055b7f5a8b6ce6332beba2d9b7e3
Karolis Liutackas <karolis.liutackas@nba-185-v1b-lt.lan>
2023-01-27 17:45:27 +0200

    added 4-5

---
+++ 4-Maps-VarLists/ec2-instance.tf
@@ -0,0 +1,14 @@
+# EC2 Instance
+resource "aws_instance" "myec2vm" {
+  ami = data.aws_ami.amzlinux2.id
+  instance_type = var.instance_type
+  #instance_type = var.instance_type_list[1] #for List to reference t3-small
+  #instance_type = var.instance_type_map["dev"] #for MAP to reference t3-micro
+  user_data = file("${path.module}/httpd-install.sh") #file function allows to read content from respective file
+  key_name = var.instance_keypair
+  vpc_security_group_ids = [ aws_security_group.vpc-ssh.id, aws_security_group.vpc-web.id   ]
+  count = 2
+  tags = {
+    "Name" = "Count-Demo-${count.index}"
+  }
+}
---
+++ 5-foreach-tomap-toset/securitygroups.tf
@@ -0,0 +1,55 @@
+# Create Security Group - SSH Traffic
+resource "aws_security_group" "vpc-ssh" { #aws_security_group - resource type, resource local name - vpc-ssh
+  name        = "vpc-ssh"
+  description = "Dev VPC SSH"
+  ingress { #inbound rule
+    description = "Allow Port 22"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] #internet edge traffic 0.0.0.0/0
+  }
+
+  egress { #outbound rule
+    description = "Allow all ip and ports outbound"    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"] #internet edge traffic 0.0.0.0/0
+  }
+
+  tags = {
+    Name = "vpc-ssh"
+  }
+}
+
+# Create Security Group - Web Traffic
+resource "aws_security_group" "vpc-web" {
+  name        = "vpc-web"
+  description = "Dev VPC Web"
+  ingress {
+    description = "Allow Port 80" #web traffic 80
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    description = "Allow Port 443" #Additional traffic rule for security group
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }  
+  egress {
+    description = "Allow all ip and ports outbound"    
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "vpc-web"
+  }
+}
