a4ac5b2d7adf256c4bfc62d4b293acab05df5f60
Karolis Liutackas <karolis.liutackas@nba-185-v1b-lt.lan>
2023-01-30 16:21:50 +0200

    added empty lines

--- 8-elb-classic-loadbalancer/ec2-instance-variables.tf
+++ 8-elb-classic-loadbalancer/ec2-instance-variables.tf
@@ -9,6 +9,8 @@ variable "instance_type" {
 }
 
 # AWS EC2 Instance Key Pair
+
+
 variable "instance_keypair" {
   description = "AWS EC2 Key pair that need to be associated with EC2 Instance"
   type = string
